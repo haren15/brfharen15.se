df27f53f9ffe052cc9a73461d127b6f3e1239a82
Johan Andersson <johan.rejeep@gmail.com>
2014-12-09 15:52:10 +0100

    Add FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/kontakt' => 'contact#index', as: 'contact'
 
   resources :news_items, path: 'nyheter'
+  resources :faqs, path: 'vanliga-fragor-och-svar'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
