7844df3f7971f0fc40c3f358c1b73b982366477b
Johan Andersson <johan.rejeep@gmail.com>
2014-12-12 10:46:12 +0100

    Restructure news items on welcome page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def render_news_item_puff(news_item)
+  def render_news_item(news_item)
     if news_item
-      render partial: 'shared/news_item_puff', locals: { news_item: news_item }
+      render partial: 'news_items/news_item', locals: { news_item: news_item }
     end
   end
 end
